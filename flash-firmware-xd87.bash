24e3960b9f4c698d14147e52b415daec78e44789
Michael Dye <mdye@us.ibm.com>
2019-07-01 15:57:38 -0600

    added xd87 script, updated upstream submodule

---
+++ flash-firmware-xd87.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export keyboard="${keyboard:=xd87}"
+export keymap="${keymap:=default}"
+
+cleanup() {
+  err=$?
+  echo "Removing docker container..."
+  docker rm -f qmk_keyboard 2> /dev/null
+  exit $err
+}
+
+trap cleanup INT EXIT
+
+docker run -d --name qmk_keyboard --privileged -v /dev:/dev -v $PWD:/qmk_keyboard:rw -t mdye/qmk_keyboard /bin/bash
+cmd="make ${keyboard}:${keymap}"
+docker exec -t qmk_keyboard bash -c "echo ***$cmd***; eval $cmd"
